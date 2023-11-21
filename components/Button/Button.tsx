78e23261ea18f8cf35f02347af372d38f990e6dd
Roger Vargas <roger.vargas@cwi.com.br>
2023-11-21 09:51:35 -0300

    feat: add storybook

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,11 +1,11 @@
-type ButtonProps = {
+export type ButtonProps = {
   children: React.ReactNode;
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
 const Button = ({ children, className }: ButtonProps) => {
   return (
     <button
-      className={`bg-[#2D5BFF] rounded-lg px-8 py-3 text-white ${className}`}
+      className={`bg-primary rounded-lg px-8 py-3 text-white ${className}`}
     >
       {children}
     </button>
